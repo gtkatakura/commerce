40aad60e64ff1eec9a73db1596d6afd90075e8a4
gtkatakura <gt.katakura@gmail.com>
2021-03-04 17:45:08 -0300

    support for queries into vtex io impl graphql

--- framework/shopify/api/utils/fetch-graphql-api.ts
+++ framework/shopify/api/utils/fetch-graphql-api.ts
@@ -1,20 +1,53 @@
 import type { GraphQLFetcher } from '@commerce/api'
 import fetch from './fetch'
 
-import { API_URL, API_TOKEN } from '../../const'
+import { API_URL, API_TOKEN, VTEX_API_URL } from '../../const'
 import { getError } from '../../utils/handle-fetch-response'
+
+export const VTEX_SUPPORTED_QUERIES: string[] = [
+  'getAllProducts',
+  'getAllProductPaths',
+  'getProductBySlug',
+]
 
 const fetchGraphqlApi: GraphQLFetcher = async (
   query: string,
   { variables } = {},
   fetchOptions
 ) => {
+  if (VTEX_SUPPORTED_QUERIES.some((queryName) => query.includes(queryName))) {
+    const res = await fetch(VTEX_API_URL, {
+      ...fetchOptions,
+      method: 'POST',
+      headers: {
+        ...fetchOptions?.headers,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    })
+
+    const { data, errors, status } = await res.json()
+
+    if (errors) {
+      throw getError(errors, status)
+    }
+
+    return { data, res }
+  }
+
+  // console.log(query, variables)
+
   const res = await fetch(API_URL, {
     ...fetchOptions,
     method: 'POST',
     headers: {
       'X-Shopify-Storefront-Access-Token': API_TOKEN!,
       ...fetchOptions?.headers,
+      Accept: 'application/json',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
@@ -24,6 +57,8 @@ const fetchGraphqlApi: GraphQLFetcher = async (
   })
 
   const { data, errors, status } = await res.json()
+
+  // console.log(JSON.stringify(data))
 
   if (errors) {
     throw getError(errors, status)
--- framework/shopify/fetcher.ts
+++ framework/shopify/fetcher.ts
@@ -1,8 +1,34 @@
 import { Fetcher } from '@commerce/utils/types'
-import { API_TOKEN, API_URL } from './const'
+import { API_TOKEN, API_URL, VTEX_API_URL } from './const'
 import { handleFetchResponse } from './utils'
 
+const VTEX_SUPPORTED_QUERIES: string[] = [
+  'getAllProducts',
+  'getAllProductPaths',
+  'getProductBySlug',
+]
+
 const fetcher: Fetcher = async ({ method = 'POST', variables, query }) => {
+  console.log('fetcher: ', query, variables)
+
+  if (
+    VTEX_SUPPORTED_QUERIES.some((queryName) =>
+      (query || '').includes(queryName)
+    )
+  ) {
+    return handleFetchResponse(
+      await fetch(VTEX_API_URL, {
+        method,
+        body: JSON.stringify({ query, variables }),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        mode: 'no-cors',
+      })
+    )
+  }
+
   return handleFetchResponse(
     await fetch(API_URL, {
       method,
